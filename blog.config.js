fb823d23254c845db03aadca40a771456fa7aaa9
SiqiZhou <70123194+zsq541012232@users.noreply.github.com>
2024-05-19 13:49:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,9 +24,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ShalTeajune', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个热爱生活的技术迷', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://947learn.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '知识分享, 博客', // 网站关键词 英文逗号隔开
   SINCE: process.env.NEXT_SINCE || 2024, // 建站年份
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
